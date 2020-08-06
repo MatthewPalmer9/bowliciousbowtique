7a4d87a31a08076dc6a09361e61fadf7f6525029
MacBookPro <macbookpro@MacBookPros-MBP.attlocal.net>
2020-08-06 11:45:04 -0500

    Got rid of navbar opacity

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ export default class Navbar extends Component {
     render() {
         return (
             <>
-                <nav className="opacity-90 navbar navbar-expand-lg navbar-light bg-light fixed-top">
+                <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
                     <a className="brand-text navbar-brand" href="/">Bowlicious Bowtique</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
